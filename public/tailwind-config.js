38f2fc0b3d83562261d527db626e28c42938cee6
Al Mumeetu Saikat <amsaikat@softzino.com>
2025-11-19 18:19:37 +0600

    update

---
+++ public/tailwind-config.js
@@ -0,0 +1,14 @@
+window.tailwind = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'hind': ['"Hind Siliguri"', 'sans-serif'],
+        'oswald': ['"Oswald"', 'sans-serif'],
+      },
+      colors: {
+        'brand-yellow': '#F3BE12', 
+        'brand-gray': '#DFE2E6', 
+      },
+    },
+  },
+};
